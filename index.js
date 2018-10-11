861b20fdfa2f952e9d91fdd62baebe811625df4e
syed m faisal <syed.mfaisal988@gmail.com>
2018-10-11 20:37:06 +0500

    callback & error

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var rect = require('./rectangle');
 function solveRect(l,b) {
     console.log('solving rectangle l=' + l + ' and b=' + b);
-    if(l <=0 || b<=0){
-        console.log("rectangle dimesion should be greater than zero");
-    }else{
-        console.log('the area of rectangle is ' + rect.area(l,b));
-        console.log('The perimeter of rectangle is ' + rect.perimeter(l,b));
-    }
-};
+    rect(l, b, (err, rectangle)=>{
+        if(err){
+            console.log(err.message);
+        }else{
+            console.log('Area of rectangle is: ' + rectangle.area() );
+            console.log('Perimeter is: ' + rectangle.perimeter());
+        }
+    });
+    console.log('This statement is after call to rect()');
+    };
 solveRect(2, 4);
 solveRect(4, 3);
